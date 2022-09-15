0f31002bd8337e8ba0665f1ec9b8c030a45f2b9a
Saki Nakazato <snkzt@mercari.com>
2022-09-15 19:27:43 +0900

    update

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 
   "github.com/go-sql-driver/mysql"
 )
+
 var db *sql.DB // Database handle. Make this global is not good in production.
+
+type Album  struct {
+  ID      int64
+  Title   string
+  Artist  string
+  Price   float
+}
 
 func main() {
   // Connection properties
@@ -37,3 +45,31 @@ func main() {
   }
   fmt.Println("Connected!")
 }
+
+// Retrieve album(s) by artist name
+func albumsByArtist(name string) ([]Album, error) {
+  var albums []Album // Type Album slice
+ 
+  // Separate query and parameter enables the database/sql package to
+  // send them separately that removes SQL injection risk compare to concatinate them.
+  rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+  if err != nil {
+    return nil, fmt.Errorf("albumByArtist %q: %v", name, err)
+  }
+  // Defer close rows so that it will release the resourse it holds at the end.
+  defer rows.Close()
+ 
+  // Loop through rows and assign column data to struct fields with Scan.
+  for rows.Next() {
+    var album Album
+    if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+      return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    albums = append(albums, album)
+  }
+  if err := rows.Err(); err != nil {
+    return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+  }
+  return albums, nil
+}
+
